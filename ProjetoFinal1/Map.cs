b1d3fa7f9d4bfde3d5c072860b7991c02c2e4209
sathyagimenes <sathya.gimenes@gmail.com>
2023-06-02 00:10:26 -0300

    refactored random map

--- ProjetoFinal1/Map.cs
+++ ProjetoFinal1/Map.cs
@@ -5,12 +5,12 @@ namespace ProjetoFinal1
 {
 	public class Map
 	{
-        Random random = new Random();
-        public IElement[,] Positions;
+		Random random = new Random();
+		public IElement[,] Positions;
 		public int Width { get; set; }
 		public int Height { get; set; }
 		public int Lvl { get; set; }
-		
+
 		public Map(int width, int height)
 		{
 			Width = width;
@@ -18,8 +18,8 @@ namespace ProjetoFinal1
 			Positions = new IElement[Width, Height];
 		}
 
-        //Adiciona os elementos no mapa no nível 1
-        public void PopulateMap()
+		//Adiciona os elementos no mapa no nível 1
+		public void PopulateMap()
 		{
 			for (int x = 0; x < Height; x++)
 			{
@@ -60,108 +60,87 @@ namespace ProjetoFinal1
 		//Adiciona os elementos no mapa a partir do nível 2
 		public void RandomMap()
 		{
-            int x,y;
-            
-            for (x = 0; x < Height; x++)
+			int x, y;
+
+			for (x = 0; x < Height; x++)
 			{
 				for (y = 0; y < Width; y++)
 				{
-					Positions[x, y] = new EmptySpace(x, y, "--");
-                }
-            }
-		
-            for (int z = 0; z < 2; z++)
+						Positions[x, y] = new EmptySpace(x, y, "--");
+				}
+			}
+
+			for (int z = 0; z < 2; z++)
 			{
-                x = random.Next(0, Height);
-                y = random.Next(0, Width);
-                Positions[x, y] = new Jewel(x, y, "JR");
+				x = random.Next(0, Height);
+				y = random.Next(0, Width);
+				if (Positions[x, y].Type == ("--"))
+					Positions[x, y] = new Jewel(x, y, "JR");
+				else
+					z--;
 			}
 			for (int c = 0; c < 2; c++)
 			{
-                x = random.Next(0, Height);
-                y = random.Next(0, Width);
-
-				if (Positions[x, y].Type != ("--"))
-				{
-					x = random.Next(0, Height);
-					y = random.Next(0, Width);
-					c--;
-				}
-				else
-				{
+				x = random.Next(0, Height);
+				y = random.Next(0, Width);
+				if (Positions[x, y].Type == ("--"))
 					Positions[x, y] = new Jewel(x, y, "JG");
-				}
+				else
+					c--;
 			}
 			for (int v = 0; v < 2; v++)
 			{
-                x = random.Next(0, Height);
-                y = random.Next(0, Width);
-				if (Positions[x, y].Type != ("--"))
-                {
-					x = random.Next(0, Height);
-					y = random.Next(0, Width);
-					v--;
-				}
-				else
-				{
+				x = random.Next(0, Height);
+				y = random.Next(0, Width);
+				if (Positions[x, y].Type == ("--"))
 					Positions[x, y] = new Jewel(x, y, "JB");
-                }
-               
+				else
+					v--;
 			}
 			for (int b = 0; b < 7; b++)
 			{
-                x = random.Next(0, Height);
-                y = random.Next(0, Width);
-				if (Positions[x, y].Type != ("--"))
-                {
-					x = random.Next(0, Height);
-					y = random.Next(0, Width);
-					b--;
-				}
-				else
-				{
+				x = random.Next(0, Height);
+				y = random.Next(0, Width);
+				if (Positions[x, y].Type == ("--"))
 					Positions[x, y] = new Obstacle(x, y, "##");
-                }
+				else
+					b--;
 			}
 			for (int m = 0; m < 5; m++)
 			{
-                x = random.Next(0, Height);
-                y = random.Next(0, Width);
-				while (Positions[x, y].Type != ("--"))
-                {
-					x = random.Next(0, Height);
-					y = random.Next(0, Width);
-				}
-				Positions[x, y] = new Obstacle(x, y, "$$");
+				x = random.Next(0, Height);
+				y = random.Next(0, Width);
+				if (Positions[x, y].Type == ("--"))
+					Positions[x, y] = new Obstacle(x, y, "$$");
+				else
+					m--;
 			}
-            for (int l = 0; l < 1; l++)
-            {
-                x = random.Next(0, Height);
-                y = random.Next(0, Width);
-				while (Positions[x, y].Type != ("--"))
-                {
-					x = random.Next(0, Height);
-					y = random.Next(0, Width);
-				}
-				Positions[x, y] = new EmptySpace(x, y, "!!");
-            }
-        }
+			for (int l = 0; l < 1; l++)
+			{
+				x = random.Next(0, Height);
+				y = random.Next(0, Width);
+				if (Positions[x, y].Type == ("--"))
+					Positions[x, y] = new EmptySpace(x, y, "!!");
+				else
+					l--;
+			}
+		}
 
 		//Responsável por gerar a matriz do mapa
 		public void PrintMap()
 		{
-			
-				for (int y = 0; y < Height; y++)
+
+			for (int y = 0; y < Height; y++)
+			{
+				for (int x = 0; x < Width; x++)
 				{
-					for (int x = 0; x < Width; x++)
-					{
-						Console.Write(Positions[x, y]);
-						Console.ResetColor();
-						Console.Write(" ");
-					}
-					Console.Write("\n");
+					Console.Write(Positions[x, y]);
+					Console.ResetColor();
+					Console.Write(" ");
 				}
-            Console.Write("\n");
-        }
+				Console.Write("\n");
+			}
+			Console.Write("\n");
+		}
 	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # WIP: Jewel Collector Game
-
-Melhorias:<br>
-transformar a map em static? <br>
-Adicionar um Enum para transformar palavra em codigo do mapa<br>
